be67427d19bfaf82d219cf857e4a1782326cd12e
Jay Hannah <jay.hannah@iinteractive.com>
2022-01-10 07:09:24 -0600

    templates 101

--- server.go
+++ server.go
@@ -3,19 +3,21 @@ package main
 // go mod init github.com/jhannah/legislature_calendar
 // go get -u github.com/gin-gonic/gin
 // go run server.go
-// http://localhost:8080/ping
+// http://localhost:8080
 
 import "github.com/gin-gonic/gin"
-//import "net/http"
+import "net/http"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router.Run(":8080")
 }
 
 
