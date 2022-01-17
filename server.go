479c51af5e639e2d6803068d9aa8fc7279b6da24
Jay Hannah <jay.hannah@iinteractive.com>
2022-01-17 13:08:21 -0600

    Go server now displaying all bills

--- server.go
+++ server.go
@@ -8,61 +8,60 @@ package main
 // http://localhost:8080
 
 import (
-  "github.com/gin-gonic/gin"
-  "net/http"
-  "gorm.io/gorm"
-  "gorm.io/driver/sqlite"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Bill struct {
-  gorm.Model
-  ID int
-  SessionID int
-  Number string
-  Status string
-  LastActionDate string
-  LastAction string
-  Title string
-  URL string
+	gorm.Model
+	ID             int
+	SessionID      int
+	Number         string
+	Status         string
+	LastActionDate string
+	LastAction     string
+	Title          string
+	URL            string
 }
 
 func main() {
-  router := gin.Default()
-  router.LoadHTMLGlob("templates/*.tmpl")
-  //router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.tmpl")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 
-  db, err := gorm.Open(sqlite.Open("leg.sqlite3"), &gorm.Config{})
-  if err != nil {
-    panic("failed to connect database")
-  }
-  var bills []Bill
-  result := db.Find(&bills)
+	db, err := gorm.Open(sqlite.Open("leg.sqlite3"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	var bills []Bill
+	result := db.Order("last_action_date desc").Find(&bills)
 
-  router.GET("/", func(c *gin.Context) {
-    c.HTML(http.StatusOK, "index.tmpl", gin.H{
-      "Title": "Main website",
-      "Bills": bills,
-      "RowsAffected": result.RowsAffected,
-      "Error": result.Error,
-    })
-  })
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Title":        "Nebraska 2021-2022 Regular Session 107th Legislature",
+			"Bills":        bills,
+			"RowsAffected": result.RowsAffected,
+			"Error":        result.Error,
+		})
+	})
 
-  router.GET("/init", func(c *gin.Context) {
-    // Migrate the schema
-    db.AutoMigrate(&Bill{})
+	router.GET("/init", func(c *gin.Context) {
+		// Migrate the schema
+		db.AutoMigrate(&Bill{})
 
-    // Create
-    db.Create(&Bill{ID: 1, SessionID: 1810, Number: "LB875", Status: "Introduced", LastActionDate: "2022-01-07", LastAction: "Date of introduction", Title: "Rename the Director-State Engineer for the Department of Transportation as the Director of Transportation for the Department of Transportation", URL: "https://legiscan.com/NE/bill/LB875/2021"})
+		// Create
+		db.Create(&Bill{ID: 1, SessionID: 1810, Number: "LB875", Status: "Introduced", LastActionDate: "2022-01-07", LastAction: "Date of introduction", Title: "Rename the Director-State Engineer for the Department of Transportation as the Director of Transportation for the Department of Transportation", URL: "https://legiscan.com/NE/bill/LB875/2021"})
 
-    c.HTML(http.StatusOK, "index.tmpl", gin.H{
-      "Title": "Main website",
-      "Bills": bills,
-      "RowsAffected": result.RowsAffected,
-      "Error": result.Error,
-    })
-  })
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Title":        "Main website",
+			"Bills":        bills,
+			"RowsAffected": result.RowsAffected,
+			"Error":        result.Error,
+		})
+	})
 
-  router.Run(":8080")
+	router.Run(":8080")
 }
-
-
