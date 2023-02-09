cb8a585f1fe141c6030cfc38b383e9ff12eaeedc
Jay Hannah <jay.hannah@iinteractive.com>
2023-02-08 19:03:44 -0600

    Only Bills. Not Appropriation Bills or Resolutions or other things.

--- experiments/CSV_dumps/2022/history_graph.pl
+++ experiments/CSV_dumps/2022/history_graph.pl
@@ -6,19 +6,24 @@ use GraphViz2;
 my $dbh = DBI->connect("dbi:SQLite:dbname=NE.sqlite3","","");
 
 my $edges;
-my $strsql = "select bill_id, action from history order by bill_id, sequence";
+my $strsql = <<EOT;
+  SELECT b.bill_number, action
+  FROM bills b, history h
+  WHERE b.bill_id = h.bill_id
+  ORDER BY h.bill_id, h.sequence
+EOT
 my $sth = $dbh->prepare($strsql);
 $sth->execute;
-my $row_cnt;
-my $prev_g;
-my $prev_bill_id;
-while (my ($bill_id, $action) = $sth->fetchrow) {
+my ($row_cnt, $prev_g, $prev_bill_number);
+while (my ($bill_number, $action) = $sth->fetchrow) {
+  # Only Bills. Not Appropriation Bills or Resolutions or other things.
+  next unless ($bill_number =~ /LB\d+$/);
   my $g = generic($action);
-  say "($prev_bill_id) $bill_id $g";
+  say "($prev_bill_number) $bill_number $g";
   # last if ($row_cnt++ > 100);
-  if ((defined $prev_bill_id) && $bill_id != $prev_bill_id) {
+  if ((defined $prev_bill_number) && $bill_number ne $prev_bill_number) {
     $prev_g = $g;
-    $prev_bill_id = $bill_id;
+    $prev_bill_number = $bill_number;
     next;
   }
   if ($prev_g) {
@@ -26,7 +31,7 @@ while (my ($bill_id, $action) = $sth->fetchrow) {
     $edges->{$prev_g}->{$g}++;
   }
   $prev_g = $g;
-  $prev_bill_id = $bill_id;
+  $prev_bill_number = $bill_number;
 }
 $sth->finish;
 $dbh->disconnect;
@@ -41,7 +46,7 @@ my $graph = GraphViz2->new(
 foreach my $from (keys %$edges) {
   foreach my $to (keys %{$edges->{$from}}) {
     my $cnt = $edges->{$from}->{$to};
-    if ($cnt > 20) {
+    if ($cnt > 19) {
       $graph->add_edge(from => $from, to => $to, label => $cnt);
     }
   }
@@ -56,12 +61,12 @@ $graph->run(
 sub generic {
   my ($action) = @_;
   $action =~ s/.* (name added|name withdrawn|point of order|priority bill)/\[Senator\] $1/;
-  $action =~ s/.* AM\d+ (adopted|filed|withdrawn|refiled|lost|reoffered|not considered|pending)/\[Senator\] \[Amendment] $1/;
+  $action =~ s/.* (AM|FA)\d+ (adopted|filed|withdrawn|refiled|lost|reoffered|not considered|pending)/\[Senator\] \[Amendment] $2/;
+  # $action =~ s/.* FA\d+ (adopted|filed|withdrawn|lost|pending|not considered)/\[Senator\] \[Floor Amendment] $1/;
   $action =~ s/.* MO\d+ (adopted|filed|withdrawn|failed|pending|Indefinitely postpone filed|Indefinitely postpone|prevailed|Invoke cloture pursuant to Rule 7, Sec\. 10 filed)/\[Senator\] \[Motion] $1/;
-  $action =~ s/.* FA\d+ (adopted|filed|withdrawn|lost|pending|not considered)/\[Senator\] \[Floor Amendment] $1/;
   $action =~ s/(Provisions\/portions of) LB.*(amended into).*/$1 \[Bill1\] $2 \[Bill2\] by \[Amendment\]/;
   $action =~ s/(Placed on General File with) AM.*/$1 \[Amendment\]/;
-  $action =~ s/(Placed on Select File with) ER.*/$1 \[Enrollement and Review\]/;
+  $action =~ s/(Placed on Select File with) ER.*/$1 \[Enrollment and Review\]/;
   $action =~ s/(Placed on Final Reading with) ST.*/$1 \[Standing Committee Amendment\]/;
   $action =~ s/(Passed on Final Reading) \d\d.*/$1 \[Vote\]/;
   $action =~ s/(Passed on Final Reading with Emergency Clause) \d\d.*/$1 \[Vote\]/;
