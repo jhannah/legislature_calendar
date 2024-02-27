de2d0b9487397b7ca7dd640ba6e27500987562c1
Jay Hannah <jay.hannah@iinteractive.com>
2024-02-27 15:22:19 -0600

    3 columns of committees

--- experiments/NE_canvas/committees.js
+++ experiments/NE_canvas/committees.js
@@ -1,17 +1,19 @@
 const committees = [
-  'Agriculture',
-  'Business and Labor',
-  'Banking, Commerce and Insurance',
-  'Health and Human Services',
-  'Executive Board',
-  'Education Committee',
-  'General Affairs',
-  'Transportation and Telecommunications',
-  'Appropriations',
-  'Nebraska Retirement Systems',
-  'Urban Affairs',
-  'Revenue',
-  'Natural Resources',
-  'Government, Military and Veterans Affairs',
-  'Judiciary'
+  {name:'Agriculture', x: 10, y: 60},
+  {name:'Business and Labor', x: 10, y: 100},
+  {name:'Banking, Commerce and Insurance', x: 10, y: 140},
+  {name:'Health and Human Services', x: 10, y: 180},
+  {name:'Executive Board', x: 10, y: 220},
+
+  {name:'Education Committee', x: 400, y: 60},
+  {name:'General Affairs', x: 400, y: 100},
+  {name:'Transportation and Telecommunications', x: 400, y: 140},
+  {name:'Appropriations', x: 400, y: 180},
+  {name:'Nebraska Retirement Systems', x: 400, y: 220},
+
+  {name:'Urban Affairs', x: 800, y: 60},
+  {name:'Revenue', x: 800, y: 100},
+  {name:'Natural Resources', x: 800, y: 140},
+  {name:'Government, Military and Veterans Affairs', x: 800, y: 180},
+  {name:'Judiciary', x: 800, y: 220}
 ];
