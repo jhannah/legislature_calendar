271cdfd9c917a833b31686f26d8011e942064e1e
Jay Hannah <jay.hannah@iinteractive.com>
2024-02-27 15:12:30 -0600

    All the committees

--- experiments/NE_canvas/README.txt
+++ experiments/NE_canvas/README.txt
@@ -2,3 +2,5 @@ sigh. CORS
 
 python3 -m http.server
 
+http://localhost:8000/index.html
+
--- experiments/NE_canvas/committees.js
+++ experiments/NE_canvas/committees.js
@@ -2,4 +2,16 @@ const committees = [
   'Agriculture',
   'Business and Labor',
   'Banking, Commerce and Insurance',
+  'Health and Human Services',
+  'Executive Board',
+  'Education Committee',
+  'General Affairs',
+  'Transportation and Telecommunications',
+  'Appropriations',
+  'Nebraska Retirement Systems',
+  'Urban Affairs',
+  'Revenue',
+  'Natural Resources',
+  'Government, Military and Veterans Affairs',
+  'Judiciary'
 ];
